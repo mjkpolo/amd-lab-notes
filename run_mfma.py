3c543bf62b1b14ac66aa5aed444d81f4ea7f3273
Marco Kurzynski <marcokurzynski@icloud.com>
2024-07-29 17:20:04 -0500

    think I actually fixed it this time?

--- run_mfma.py
+++ run_mfma.py
@@ -2,12 +2,16 @@
 
 import subprocess
 from pathlib import Path
+from os import chdir, getcwd
 
 native = True
 
 
 def runcmd(*args):
+    pd = getcwd()
+    chdir('..')
     subprocess.Popen(args).communicate()
+    chdir(pd)
 
 
 def rundocker(*args):
@@ -18,16 +22,16 @@ def rundocker(*args):
                "--rm",
                "--privileged",
                "-t",
-               "-v", ".:/gem5",
-               "-w", "/gem5",
+               "-v", ".:/mnt",
+               "-w", "/mnt",
                container_name,
                "bash", "-c", " ".join(args))
 
 
 container_name = "mi200-container"
-dockerfile_dir = "../gem5/util/dockerfiles/gpu-fs"
-bench_dir = Path("matrix-cores")
-resources = Path("../gem5-resources/src/x86-ubuntu-gpu-ml")
+dockerfile_dir = "gem5/util/dockerfiles/gpu-fs"
+bench_dir = Path("amd-lab-notes/matrix-cores")
+resources = Path("gem5-resources/src/x86-ubuntu-gpu-ml")
 
 
 def build_docker():
@@ -43,15 +47,15 @@ def main():
     if not native:
         build_docker()
     compile()
-    bins = (tuple(file.stem for file in (bench_dir / 'src').iterdir()
+    bins = (tuple(file.stem for file in ('..' / bench_dir / 'src').iterdir()
                   if file.suffix == '.cpp'))
     for bin in bins:
         print(f"running {bin}:")
         if native:
             runcmd(bench_dir/bin)
         else:
-            rundocker("../gem5/build/VEGA_X86/gem5.opt",
-                      "configs/example/gpufs/mi200.py",
+            rundocker("gem5/build/VEGA_X86/gem5.opt",
+                      "gem5/configs/example/gpufs/mi200.py",
                       "-a",
                       str(bench_dir/bin),
                       "--kernel",
