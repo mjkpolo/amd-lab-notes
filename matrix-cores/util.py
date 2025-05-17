d5a07cc5fcd99d9a5dd7bef92b874d067a1324c5
Marco Kurzynski <marcokurzynski@icloud.com>
2025-05-17 12:11:43 -0500

    add comparison to f_fc_dp flops

--- matrix-cores/util.py
+++ matrix-cores/util.py
@@ -14,7 +14,7 @@ def main():
     full_mfma = get_csv("FULL_MFMA")
     startup_mfma = get_csv("STARTUP_MFMA")
 
-    flops = 4096*4096*14336*2
+    dp_flops = 4096*4096*14336*2
     flops_per_mfma = 16*16*16*304*4*2
 
     full_df = pd.read_csv(full_mfma)
@@ -37,8 +37,11 @@ def main():
 
     startup_cycles = get_counter_value(startup_df, 'GRBM_GUI_ACTIVE')
 
-    print('unadjusted:', get_mfma_util(full_df))
-    print('no startup:', get_mfma_util(full_df, startup_cycles))
+    print('unadjusted mfma util %:', get_mfma_util(full_df))
+    print('no startup mfma util %:', get_mfma_util(full_df, startup_cycles))
+
+    actual_flops = get_counter_value(full_df, 'SQ_INSTS_VALU_MFMA_MOPS_F16')*512
+    print(f'% diff to f_fc_dp flops: {(dp_flops-actual_flops)/dp_flops*100}')
 
 
 if __name__ == "__main__":
